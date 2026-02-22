8d900b77e4c194524a11b7dcad96caf7e83169c2
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-22 10:34:31 +0530

    Refactor comments and remove print statements

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
 # AIM: Write a Python program to create,
-# update, and manipulate a dictionary of student records,
-# including their grades and attendance.
-# Coder:Shamal 
-# Date:4/2/26
+# update, and manipulate a dictionary of student 
 
-print("--- Student Records Manager ---")
-students_records = {
-    "251P005" : {"name":"Sameer","grade":"A","attendance": 68},
-    "251P055" : {"name":"Abdulla","grade":"B+","attendance": 88},
-    "251P026" : {"name":"Katrina","grade":"A-","attendance": 55},
-}
-
-print(f"Current Student Records: {students_records}")
-
-# Write your code here
-#TODO: Add a new Student record
-uin = input("Enter New Student UIN: ")
-name = input("Enter New Student Name: ")
-grade = input("Enter New Student Grade: ")
-attendance = int(input("Enter New Student Attendance: "))
-
-students_records[uin] = {
-    "name": name,
-    "grade": grade,
-    "attendance": attendance
-}
-
-#TODO: Update the Grade of a Student of UIN 251P055 
-update_uin = input("Enter UIN to Update: ")
-new_grade = input("Enter New Grade of Student: ")
-students_records[update_uin]["grade"] = new_grade
-
-#TODO: Remove Student with UIN 251P026 
-delete_uin = input("Enter UIN of the Student to Delete: ")
-delete students_records[delete_uin]
-
-
-# Stop coding here
-print(f"Final Student Records: {students_records}")
 
 
 
