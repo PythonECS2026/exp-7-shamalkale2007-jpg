38b9c21ee5bad04b62d4bc4e2525efbbf76eb9f2
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-22 09:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,25 @@ students_records[update_uin]["grade"] = new_grade
 delete_uin = input("Enter UIN of the Student to Delete: ")
 delete students_records[delete_uin]e your code here
 # TODO: Add a new Student Record 
+uin = input("Enter New Student UIN: ")
+name = input("Enter New Student Name: ")
+grade = input("Enter New Student Grade: ")
+attendance = int(input("Enter New Student Attendance: "))
 
+students_records[uin] = {
+    "name": name,
+    "grade": grade,
+    "attendance": attendance
+}
 # TODO: Update the Grade of Student of UIN 251P055
-
+update_uin = input("Enter UIN to Update: ")
+new_grade = input("Enter New Grade of Student: ")
+students_records[update_uin]["grade"] = new_grade
 # TODO: Remove Student with UIN 251P026
-
+delete_uin = input("Enter UIN of the Student to Delete: ")
+delete students_records[delete_uin]e your code here
 
 # Stop coding here
 print(f"Final Student Records: {students_records}")
+
 
