c0ffa23bbb07262eff40cb7a9c85d44de4f9555d
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-22 10:03:29 +0530

    Refactor student records management code

--- main.py
+++ main.py
@@ -11,28 +11,7 @@ students_records = {
     "251P026" : {"name":"Katrina","grade":"A-","attendance": 55},
 }
 
-print(f"Current Student Records: {students_records}")
-
-# Writuin = input("Enter New Student UIN: ")
-name = input("Enter New Student Name: ")
-grade = input("Enter New Student Grade: ")
-attendance = int(input("Enter New Student Attendance: "))
-
-students_records[uin] = {
-    "name": name,
-    "grade": grade,
-    "attendance": attendance
-}
-
-#Update the Grade of Student of UIN 251P055
-update_uin = input("Enter UIN to Update: ")
-new_grade = input("Enter New Grade of Student: ")
-students_records[update_uin]["grade"] = new_grade
-
-#Remove Student with UIN 251P026
-delete_uin = input("Enter UIN of the Student to Delete: ")
-delete students_records[delete_uin]e your code here
-# TODO: Add a new Student Record 
+# -------- Add a New Student --------
 uin = input("Enter New Student UIN: ")
 name = input("Enter New Student Name: ")
 grade = input("Enter New Student Grade: ")
@@ -43,16 +22,20 @@ students_records[uin] = {
     "grade": grade,
     "attendance": attendance
 }
-# TODO: Update the Grade of Student of UIN 251P005
+
+# -------- Update Grade of a Student --------
 update_uin = input("Enter UIN to Update: ")
 new_grade = input("Enter New Grade of Student: ")
 students_records[update_uin]["grade"] = new_grade
-# TODO: Remove Student with UIN 251P055
+
+# -------- Delete a Student --------
 delete_uin = input("Enter UIN of the Student to Delete: ")
-delete students_records[delete_uin]e your code here
+delete students_records[delete_uin]
+
 
 # Stop coding here
 print(f"Final Student Records: {students_records}")
+
 
 
 
